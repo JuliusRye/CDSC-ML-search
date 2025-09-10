9550efea67fc43d961e02303fd1ef8bd9a8679da
JuliusRye <juliusrye@gmail.com>
2025-09-10 15:08:35 +0200

    Added core modules from my master thesis and bagan on writing tests

---
+++ experiments/unit-test/test_neural_network.py
@@ -0,0 +1,133 @@
+import sys, os
+
+import jax
+sys.path.append(os.path.abspath(
+    os.getcwd()+"/experiments")
+)
+
+import tempfile
+import jax.numpy as jnp
+from jax import random
+from src.neural_network import MLP, CNN, CNNDual, CNNDecoder, save_params, load_params
+
+def test_mlp_forward():
+	# Arrange
+	model = MLP(layer_sizes=[4, 6, 4, 5])
+	x_single = jnp.array([2.2, 5.1, 1.6, 6.2])
+	x_batch = jnp.array([[2.2, 5.1, 1.6, 6.2], [2.2, 5.5, 1.6, 6.2]])
+	key = random.key(0)
+	# Act
+	params = model.init(key)
+	out_single = model.apply_single(params, x_single)
+	out_batch = model.apply_batch(params, x_batch)
+	# Assert
+	assert out_single.shape == (5,)
+	assert out_batch.shape == (2, 5)
+
+def test_cnn_forward():
+	# Arrange
+	key = random.key(1)
+	input_shape=(3, 5, 5)
+	conv_layers=[(1, 3, 2, 2)]
+	batch_size = 2
+	channels = 3
+	# Act
+	model = CNN(input_shape=input_shape, conv_layers=conv_layers)
+	params = model.init(key)
+	img = random.uniform(key, shape=(batch_size, channels, 5, 5))
+	out = model.apply_batch(params, img)
+	# Assert
+	assert out.shape == (batch_size, 1, 4, 4)
+
+def test_cnndual_forward():
+	# Arrange
+	key = random.key(2)
+	input_shape_1=(1, 4, 4)
+	input_shape_2=(6, 3, 3)
+	conv_layers_input_1=[(5, 2, 1, 0)]
+	conv_layers_input_2=[(5, 1, 1, 0)]
+	conv_layers_stage_2=[(10, 2, 1, 0)]
+	fc_layers=[25, 5]
+	batch_size = 2
+	# Act
+	model = CNNDual(
+		input_shape_1,
+		input_shape_2,
+		conv_layers_input_1,
+		conv_layers_input_2,
+		conv_layers_stage_2,
+		fc_layers,
+	)
+	params = model.init(key)
+	img_prim = random.uniform(key, shape=(batch_size, 1, 4, 4))
+	img_seco = random.uniform(key, shape=(batch_size, 6, 3, 3))
+	out_batch = model.apply_batch(params, img_prim, img_seco)
+	# Assert
+	assert out_batch.shape == (batch_size, 5)
+
+def test_cnndecoder_forward():
+	# Arrange
+	key = random.key(3)
+	input_shape=(2, 4, 4)
+	conv_layers=[(2, 2, 1, 0)]
+	fc_layers=[8, 3]
+	batch_size = 2
+	# Act
+	model = CNNDecoder(
+		input_shape,
+		conv_layers,
+		fc_layers
+	)
+	params = model.init(key)
+	img = random.uniform(key, shape=(batch_size, 2, 4, 4))
+	out_batch = model.apply_batch(params, img)
+	# Assert
+	assert out_batch.shape == (batch_size, 3)
+
+def _params_equal(p1, p2):
+    return jax.tree_util.tree_all(
+        jax.tree_util.tree_map(lambda a, b: jnp.allclose(a, b), p1, p2)
+    )
+
+def test_save_and_load_params():
+	# Arrange
+	key = random.key(3)
+	input_shape=(2, 4, 4)
+	conv_layers=[(2, 2, 1, 0)]
+	fc_layers=[8, 3]
+	settings = {
+        "TRAINING_BATCHES": 1000,
+        "BATCH_SIZE": 32,
+        "SEED": 3,
+        "INIT_LEARNING_RATE": 1e-3,
+        "LEARNING_RATE": 1e-4,
+        "WARMUP_STEPS": 100,
+        "TRANSITION_STEP": 200,
+        "DECAY_RATE": 0.1,
+        "CODE_DISTANCE": 5,
+        "ERROR_PROBS": 0.01,
+        "CONV_LAYERS": [[2, 2, 1, 0]],
+        "FC_LAYERS": [8, 3],
+    }
+	# Act
+	model = CNNDecoder(
+		input_shape,
+		conv_layers,
+		fc_layers
+	)
+	params = model.init(key)
+	# Assert
+	with tempfile.TemporaryDirectory() as tmpdir:
+		file_name = os.path.join(tmpdir, 'params.json')
+		save_params(params, settings, file_name)
+		assert os.path.exists(file_name)
+		loaded_settings, loaded_params = load_params(file_name)
+		assert _params_equal(loaded_params, params)
+		assert _params_equal(loaded_settings, settings) 
+
+if __name__ == "__main__":
+    # test_mlp_forward()
+    # test_cnn_forward()
+    test_cnndual_forward()
+    # test_cnndecoder_forward()
+    # test_save_and_load_params()
---
+++ experiments/unit-test/test_quantum_error_correction_code.py
@@ -0,0 +1,88 @@
+import sys, os
+sys.path.append(os.path.abspath(
+    os.getcwd()+"/experiments")
+)
+
+import pytest
+import jax.numpy as jnp
+from jax import random
+from src.quantum_error_correction_code import QEC, SurfaceCode
+
+def test_surface_code():
+    # Arrange
+    L = 5
+    # Act
+    qec = SurfaceCode(L=L)
+    # Assert
+    assert qec.num_syndrome_qubits == 24
+    assert qec.num_data_qubits == 25
+    assert qec.lx_original.shape == (2, 25)
+    assert qec.lz_original.shape == (2, 25)
+    assert qec.hx_original.shape == (24, 25)
+    assert qec.hz_original.shape == (24, 25)
+
+def test_random_deformation():
+    # Arrange
+    qec = SurfaceCode(L=5)
+    key = random.key(0)
+    allowed = jnp.array([0, 1, 2])
+    # Act
+    deformation_1, new_key = qec.random_deformation(key, allowed)
+    deformation_2, new_key = qec.random_deformation(key, allowed)
+    # Assert
+    assert deformation_1.shape[0] == qec.num_data_qubits
+    assert jnp.all(deformation_1 == deformation_2) # check determinism
+    assert jnp.all(jnp.isin(deformation_1, allowed))
+    assert new_key != key
+
+def test_deformation_parity_info():
+    # Arrange
+    qec = SurfaceCode(L=5)
+    deformation = jnp.array([0,3,1,3,2,1,0,2,0,1,2,3,1,2,3,0,1,2,3,0,1,2,3,0,1])  # Example deformation
+    # Act
+    hx, hz, lx, lz = qec.deformation_parity_info(deformation)
+    # Assert
+    assert hx.shape == (24, 25)
+    assert hz.shape == (24, 25)
+    assert lx.shape == (2, 25)
+    assert lz.shape == (2, 25)
+
+def test_error():
+    # Arrange
+    qec = SurfaceCode(L=5)
+    key = random.key(0)
+    px, py, pz = 0.1, 0.2, 0.3
+    # Act
+    errors = qec.error(key, [px, py, pz])
+    # Assert
+    assert errors.shape == (2, 25)
+
+def test_syndrome():
+    # Arrange
+    qec = SurfaceCode(L=5)
+    error = jnp.array([
+        [1,0,0,0,0, 0,0,0,0,0, 0,0,1,0,0, 0,0,0,0,0, 0,0,0,0,0],  # X errors
+        [0,1,0,0,0, 0,0,0,1,0, 0,0,0,0,0, 0,1,0,0,0, 0,0,0,0,1],  # Z errors
+    ])
+    # Act
+    parity_info = qec.deformation_parity_info(jnp.zeros(qec.num_data_qubits, dtype=int))
+    syndrome, logical_1 = qec.syndrome(error, parity_info)
+    syndrome_img, logical_2 = qec.syndrome_img(error, parity_info)
+    # Assert
+    assert syndrome.shape == (24,)
+    assert syndrome_img.shape == (6,6)
+    assert logical_1.shape == (2,)
+    assert jnp.all(logical_1 == logical_2)
+
+def test_deformation_image():
+    # Arrange
+    qec = SurfaceCode(L=5)
+    deformation = jnp.array([0,3,1,3,2,1,0,2,0,1,2,3,1,2,3,0,1,2,3,0,1,2,3,0,1])  # Example deformation
+    # Act
+    deformation_img = qec.deformation_image(deformation)
+    # Assert
+    assert deformation_img.shape == (6,5,5)
+
+if __name__ == "__main__":
+    test_random_deformation()
+    test_deformation_parity_info()
